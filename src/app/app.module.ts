ef3e4efda9171cc2499315529e4aaace27a4baf5
Developer <developer@IM-L12-1272.local>
2018-05-29 15:45:43 -0500

    navbar attemp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AlertModule } from 'ngx-bootstrap';
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AlertModule.forRoot()],
+  imports: [BrowserModule, CollapseModule.forRoot(), BsDropdownModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
